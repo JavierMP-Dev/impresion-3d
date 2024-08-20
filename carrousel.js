bde59721c29c569d6a041365104e82d7a584c9ac
Javier <montoya.javier.15@hotmail.com>
2024-08-19 18:38:37 -0600

    cambios en las imagenes

--- carrousel.js
+++ carrousel.js
@@ -1,29 +1,36 @@
-let currentSlide = 0;
+let currentSlides = {};
 
-function showSlide(index) {
-    const slides = document.querySelectorAll('.carousel-item');
+function showSlide(carouselId, index) {
+    const carousel = document.getElementById(carouselId);
+    const slides = carousel.querySelectorAll('.carousel-item');
+
     if (index >= slides.length) {
-        currentSlide = 0;
+        currentSlides[carouselId] = 0;
     } else if (index < 0) {
-        currentSlide = slides.length - 1;
+        currentSlides[carouselId] = slides.length - 1;
     } else {
-        currentSlide = index;
+        currentSlides[carouselId] = index;
     }
 
-    const offset = -currentSlide * 100;
-    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+    const offset = -currentSlides[carouselId] * 100;
+    carousel.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
 
     slides.forEach(slide => slide.classList.remove('active'));
-    slides[currentSlide].classList.add('active');
+    slides[currentSlides[carouselId]].classList.add('active');
 }
 
-function nextSlide() {
-    showSlide(currentSlide + 1);
+function nextSlide(carouselId) {
+    showSlide(carouselId, currentSlides[carouselId] + 1);
 }
 
-function prevSlide() {
-    showSlide(currentSlide - 1);
+function prevSlide(carouselId) {
+    showSlide(carouselId, currentSlides[carouselId] - 1);
 }
 
-// Iniciando el carrusel
-showSlide(currentSlide);
+// Inicializando todos los carruseles en la página
+document.querySelectorAll('.carousel').forEach(carousel => {
+    const carouselId = carousel.getAttribute('id');
+    currentSlides[carouselId] = 0;
+    showSlide(carouselId, 0);
+});
+
